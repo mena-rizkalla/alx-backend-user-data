d6b6ae1b82139ff40adb0a6d6066779464b84542
mena-rizkalla <mena.rizkalla1@gmail.com>
2024-08-10 05:29:02 +0300

    app

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -14,6 +14,26 @@ app.register_blueprint(app_views)
 CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
 
 
+@app.before_request
+def before_request():
+    """ Handle authentication before each request."""
+    if auth is None:
+        return
+
+    # Paths that do not require authentication
+    excluded_paths = ['/api/v1/status/',
+                      '/api/v1/unauthorized/', '/api/v1/forbidden/']
+
+    if not auth.require_auth(request.path, excluded_paths):
+        return
+
+    if auth.authorization_header(request) is None:
+        abort(401)
+
+    if auth.current_user(request) is None:
+        abort(403)
+
+
 @app.errorhandler(404)
 def not_found(error) -> str:
     """ Not found handler
