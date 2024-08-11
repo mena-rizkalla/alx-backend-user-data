2559f51bbe0217019aee8a58e88e4357b48f2c19
Mina Rizkalla Azmy <52071472+mena-rizkalla@users.noreply.github.com>
2024-08-11 05:23:46 +0300

    Update session_auth.py

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -1,37 +1,42 @@
 #!/usr/bin/env python3
-""" Session auth
+"""Session authentication.
 """
-
-import uuid import uuid4
+from uuid import uuid4
 from flask import request
-from api.v1.auth.auth import Auth
+
+from .auth import Auth
 from models.user import User
 
 
 class SessionAuth(Auth):
-    """ Session Authentication class. """
-
+    """Session authentication class.
+    """
     user_id_by_session_id = {}
 
     def create_session(self, user_id: str = None) -> str:
-        """ Creates a Session ID for a user_id. """
+        """Creates a session id for the user.
+        """
         if type(user_id) is str:
             session_id = str(uuid4())
             self.user_id_by_session_id[session_id] = user_id
             return session_id
 
     def user_id_for_session_id(self, session_id: str = None) -> str:
-        """Returns the user_id for a sesion_id. """
+        """Retrieves the user id of the user associated with
+        a given session id.
+        """
         if type(session_id) is str:
             return self.user_id_by_session_id.get(session_id)
 
-    def current_user(self, request=None):
-        """ Returns a User instance based on a cookie value. """
+    def current_user(self, request=None) -> User:
+        """Retrieves the user associated with the request.
+        """
         user_id = self.user_id_for_session_id(self.session_cookie(request))
         return User.get(user_id)
 
     def destroy_session(self, request=None):
-        """ Destroys the user session and logout. """
+        """Destroys an authenticated session.
+        """
         session_id = self.session_cookie(request)
         user_id = self.user_id_for_session_id(session_id)
         if (request is None or session_id is None) or user_id is None:
