cd7e4504c8efa2b7dd565a47c7ee3c36ba313eed
mena-rizkalla <mena.rizkalla1@gmail.com>
2024-08-15 09:16:37 +0300

    create User model

---
+++ 0x03-user_authentication_service/user.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""The user model
+"""
+
+from sqlalchemy import Column, Integer, String
+
+
+class User(Base):
+    __tablename__ = 'users'
+    id = Column(Integer, primary_key=True)
+    email = Column(String, nullable=False)
+    hashed_password = Column(String, nullable=False)
+    session_id = Column(String, nullable=True)
+    reset_token = Column(String, nullable=True)
